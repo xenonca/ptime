87aed4ca0785fd6b3f5cd201ddbb69528b2780d8
Xenon <36995016+xenonca@users.noreply.github.com>
2020-08-05 09:47:59 +0200

    Log improvements

--- init.lua
+++ init.lua
@@ -2,9 +2,9 @@
 local modpath = minetest.get_modpath("ptime").. DIR_DELIM
 
 if minetest.global_exists("unified_inventory") then
-	minetest.log("ptime using unified_inventory")
+	minetest.log("action", "ptime using unified_inventory")
 	dofile(modpath .. "ptime_ui/init.lua")
 else
-	minetest.log("ptime not using unified_inventory")
+	minetest.log("action", "ptime not using unified_inventory")
 	dofile(modpath .. "ptime/init.lua")
 end
