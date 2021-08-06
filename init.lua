e04dd8fce22d349e4794f29dc86d2530b31f6862
wsor <24964441+wsor4035@users.noreply.github.com>
2021-08-06 16:32:05 -0400

    remove depreciated api methods

--- init.lua
+++ init.lua
@@ -1,19 +1,21 @@
 minetest.register_privilege("daylight", {
-    description = "Can set Perma Time"
+    description = "Can set Perma Time",
+    give_to_singleplayer = false,
 })
 
 local function ptime(name)
 	local player = minetest.get_player_by_name(name)
-	if player:get_attribute("ptime") == nil or player:get_attribute("ptime") == "" then
-		player:set_attribute("ptime", "day")
+    local pmeta = player:get_meta()
+	if not pmeta:get("ptime") then
+		pmeta:set_string("ptime", "day")
 		player:override_day_night_ratio(1)
 		minetest.chat_send_player(name, "-!- Perma Day has been enabled")
-    elseif player:get_attribute("ptime") == "day" then
-		player:set_attribute("ptime", "night")
+    elseif pmeta:get("ptime") == "day" then
+		pmeta:set_string("ptime", "night")
 		player:override_day_night_ratio(.1)
 		minetest.chat_send_player(name, "-!- Perma Night has been enabled")
-    elseif player:get_attribute("ptime") == "night" then
-		player:set_attribute("ptime", nil)
+    elseif pmeta:get("ptime") == "night" then
+		pmeta:set_string("ptime", nil)
 		player:override_day_night_ratio(nil)
 		minetest.chat_send_player(name, "-!- Perma Time has been disabled")
     end
@@ -25,22 +27,22 @@ minetest.register_chatcommand("ptime", {
     },
     description = ("Set a permanent day time"),
     func = function(name)
-        local player = minetest.get_player_by_name(name)
-        if player == nil then
-            return false, "Player does not exist!"
+        if not minetest.get_player_by_name(name) then
+            return false, "-!- Player does not exist!"
         end
         ptime(name)
     end
 })
 
 minetest.register_on_joinplayer(function(player)
-	local player_name = player:get_player_name()
-	if player:get_attribute("ptime") == "day" then
+	local pname = player:get_player_name()
+    local pmeta = player:get_meta()
+	if pmeta:get_string("ptime") == "day" then
         player:override_day_night_ratio(1)
-	    minetest.chat_send_player(player_name, "-!- Perma Day is enabled")
-	elseif player:get_attribute("ptime") == "night" then
+	    minetest.chat_send_player(pname, "-!- Perma Day is enabled")
+	elseif pmeta:get_string("ptime") == "night" then
 	    player:override_day_night_ratio(".1")
-	    minetest.chat_send_player(player_name, "-!- Perma Night is enabled")
+	    minetest.chat_send_player(pname, "-!- Perma Night is enabled")
     end
 end)
 
@@ -52,22 +54,10 @@ if minetest.global_exists("unified_inventory") then
         hide_lite=true,
         action = function(player)
             local player_name = player:get_player_name()
-            if minetest.check_player_privs(player_name, {daylight=true}) then
-                if player:get_attribute("ptime") == nil or player:get_attribute("ptime") == "" then
-                    player:set_attribute("ptime", "day")
-                    player:override_day_night_ratio(1)
-                    minetest.chat_send_player(player_name, ("-!- Perma Day enabled"))
-                elseif player:get_attribute("ptime") == "day" then
-                    player:set_attribute("ptime", "night")
-                    player:override_day_night_ratio(.1)
-                    minetest.chat_send_player(player_name, ("-!- Perma Night enabled"))
-                elseif player:get_attribute("ptime") == "night" then
-                    player:set_attribute("ptime", "")
-                    player:override_day_night_ratio(nil)
-                    minetest.chat_send_player(player_name, ("-!- Perma Time disabled"))
-                end
-            else
-                minetest.chat_send_player(player_name, ("You don't have the \"daylight\" privilege!"))
+            if not minetest.check_player_privs(player_name, {daylight=true}) then
+                minetest.chat_send_player(player_name, "-!- You don't have the \"daylight\" privilege!")
+                return
             end
+            ptime(player_name)
     end})
 end
